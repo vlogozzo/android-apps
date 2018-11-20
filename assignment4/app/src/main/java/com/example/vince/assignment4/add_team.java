18bb9fa3f3d93049e392bc3ffe9f8aa28ef2c8d1
vlogozzo <vincentwow@hotmail.com>
2018-11-20 00:08:02 -0500

    moved duplicate code from assignment 3 to assignment 4

---
+++ assignment4/app/src/main/java/com/example/vince/assignment4/add_team.java
@@ -0,0 +1,82 @@
+package com.example.vince.assignment4;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+
+public class add_team extends AppCompatActivity {
+    private byte[] imageData = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_team);
+        Log.d("inspect", "activity started");
+//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image_not_found);
+//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//        bitmap.compress(Bitmap.CompressFormat.PNG, 20, byteArrayOutputStream);
+//        imageData = byteArrayOutputStream.toByteArray();
+        //todo: check if xml sizing is effecting set image
+        ImageView test = findViewById(R.id.teamImageView);
+        test.setImageResource(R.drawable.image_not_found);
+
+        findViewById(R.id.uploadImageButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+
+        findViewById(R.id.submitButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView city = findViewById(R.id.cityField);
+                TextView name = findViewById(R.id.nameField);
+                Spinner sport = findViewById(R.id.sportSpinner);
+                TextView mvp = findViewById(R.id.mvpField);
+                ImageView image = findViewById(R.id.teamImageView);
+
+
+                if (city.getText().length() > 0 && name.getText().length() > 0) {
+                    Intent intent = new Intent();
+                    Bundle bundle = new Bundle();
+
+//                    bundle.putParcelable("newTeam", new Team(
+//                            city.getText().toString(),
+//                            name.getText().toString(),
+//                            sport.getSelectedItem().toString(),
+//                            mvp.getText().toString(),
+//                            imageData
+//                    ));
+
+                    intent.putExtra("newTeam", bundle);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "City and Name are required", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        findViewById(R.id.exitButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(RESULT_CANCELED);
+                finish();
+            }
+        });
+    }
+}
+
---
+++ assignment4/app/src/main/java/com/example/vince/assignment4/listAdapter.java
@@ -0,0 +1,33 @@
+package com.example.vince.assignment4;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class listAdapter extends ArrayAdapter<Team> {
+    private Activity mainActivity;
+    private ArrayList<Team> teams;
+
+    public listAdapter(@NonNull Activity context, @NonNull List<Team> objects) {
+        super(context, R.layout.list_element, objects);
+        mainActivity = context;
+        teams = (ArrayList<Team>) objects;
+    }
+
+    @NonNull
+    public View getView(int position, View view, @NonNull ViewGroup parent) {
+        if (view == null)
+            view = mainActivity.getLayoutInflater().inflate(R.layout.list_element, parent, false);
+
+        ((TextView) view.findViewById(R.id.textField1)).setText(teams.get(position).getCity());
+        ((TextView) view.findViewById(R.id.textField2)).setText(teams.get(position).getName());
+        return view;
+    }
+}
