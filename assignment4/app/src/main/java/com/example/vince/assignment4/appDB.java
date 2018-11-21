9d49533ec16687fd06726ea7a7ff03533f39e102
vlogozzo <vincentwow@hotmail.com>
2018-11-21 00:14:36 -0500

    database now supports deleting random teams from the list

--- assignment4/app/src/main/java/com/example/vince/assignment4/appDB.java
+++ assignment4/app/src/main/java/com/example/vince/assignment4/appDB.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import java.util.ArrayList;
 
@@ -16,7 +17,7 @@ public class appDB extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table if not exists teams(id INTEGER PRIMARY KEY, city TEXT, name TEXT, sport TEXT, mvp TEXT, stadium TEXT)");
+        db.execSQL("create table if not exists teams(city TEXT, name TEXT PRIMARY KEY, sport TEXT, mvp TEXT, image TEXT)");
     }
 
     @Override
@@ -30,14 +31,13 @@ public class appDB extends SQLiteOpenHelper {
 
         Cursor cursor = getReadableDatabase().rawQuery("SELECT * FROM teams", null);
         while (cursor.moveToNext()) {
-//            teams.add(new Team(
-//                    cursor.getInt(cursor.getColumnIndex("id")),
-//                    cursor.getString(cursor.getColumnIndex("city")),
-//                    cursor.getString(cursor.getColumnIndex("name")),
-//                    cursor.getString(cursor.getColumnIndex("sport")),
-//                    cursor.getString(cursor.getColumnIndex("mvp")),
-//                    cursor.getString(cursor.getColumnIndex("stadium"))
-//            ));
+            teams.add(new Team(
+                    cursor.getString(cursor.getColumnIndex("city")),
+                    cursor.getString(cursor.getColumnIndex("name")),
+                    cursor.getString(cursor.getColumnIndex("sport")),
+                    cursor.getString(cursor.getColumnIndex("mvp")),
+                    cursor.getString(cursor.getColumnIndex("image"))
+            ));
         }
         return teams;
     }
@@ -45,16 +45,22 @@ public class appDB extends SQLiteOpenHelper {
     public void update_teams(ArrayList<Team> teams) {
         SQLiteDatabase db = getWritableDatabase();
         for (Team t : teams) {
-//            ContentValues values = new ContentValues();
-//            if (t.getId() != 0)
-//                values.put("id", t.getId());
-//            values.put("city", t.getCity());
-//            values.put("name", t.getName());
-//            values.put("sport", t.getSport());
-//            values.put("mvp", t.getMvp());
-//            values.put("stadium", t.getStadium());
-//            db.insertWithOnConflict("teams", null, values, SQLiteDatabase.CONFLICT_REPLACE);
+            Log.d("inspect", "update_teams: " + t.toString());
+            ContentValues values = new ContentValues();
+            values.put("city", t.getCity());
+            values.put("name", t.getName());
+            values.put("sport", t.getSport());
+            values.put("mvp", t.getMvp());
+            values.put("image", t.getImage());
+            db.insertWithOnConflict("teams", null, values, SQLiteDatabase.CONFLICT_REPLACE);
         }
+        db.close();
+    }
+
+    public void delete_team(String name) {
+        SQLiteDatabase db = getWritableDatabase();
+        StringBuilder sb = new StringBuilder().append("name = '").append(name).append("'");
+        db.delete("teams", sb.toString(), null);
         db.close();
     }
 }
