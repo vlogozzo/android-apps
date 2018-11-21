a40804374e84df3248745e17e9f9603944ed2a2d
vlogozzo <vincentwow@hotmail.com>
2018-11-21 00:08:45 -0500

    updated main activity to handle update and delete sent from other activity

--- assignment4/app/src/main/java/com/example/vince/assignment4/MainActivity.java
+++ assignment4/app/src/main/java/com/example/vince/assignment4/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.vince.assignment4;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -25,39 +24,47 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == ADD_TEAM_REQUEST && resultCode == RESULT_OK && data != null) {
             Bundle bundle = data.getBundleExtra("newTeam");
             Team temp = bundle.getParcelable("newTeam");
-
             teams.add(temp);
             adapter.notifyDataSetChanged();
         } else if (requestCode == UPDATE_TEAM_REQUEST && resultCode == RESULT_OK && data != null) {
             Bundle bundle = data.getBundleExtra("updatedTeam");
             teams.set(bundle.getInt("index"), (Team) bundle.getParcelable("updatedTeam"));
             adapter.notifyDataSetChanged();
+        } else if (requestCode == UPDATE_TEAM_REQUEST && resultCode == 2 && data != null) {
+            Bundle bundle = data.getBundleExtra("team");
+            Team temp = bundle.getParcelable("team");
+
+            for (int i = 0; i < teams.size(); i++)
+                if (teams.get(i).getName().equals(temp.getName())) {
+                    teams.remove(i);
+                    db.delete_team(temp.getName());
+                    break;
+                }
+            adapter.notifyDataSetChanged();
         } else if (resultCode == RESULT_CANCELED) {
             //activity sends nothing back (exit button hit)
         }
-    }
 
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        teams = db.getAllTeams();
-//        Log.d("checkout", "onCreate: "+String.valueOf(teams.size()));
-//        Team.setCount(teams.size());
-//        adapter = new listAdapter(this, teams);
-//        ListView listview = findViewById(R.id.listView);
-//        listview.setAdapter(adapter);
-//        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Intent intent = new Intent(MainActivity.this, update_team.class);
-//                intent.putExtra("team", teams.get(position));
-//                intent.putExtra("index", position);
-//                startActivityForResult(intent, UPDATE_TEAM_REQUEST);
-//            }
-//        });
+        teams = db.getAllTeams();
+        adapter = new listAdapter(this, teams);
+        ListView listview = findViewById(R.id.listView);
+        listview.setAdapter(adapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, update_team.class);
+                intent.putExtra("team", teams.get(position));
+                intent.putExtra("index", position);
+                startActivityForResult(intent, UPDATE_TEAM_REQUEST);
+            }
+        });
 
         findViewById(R.id.addTeamButton).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        //db.update_teams(teams);
+        db.update_teams(teams);
     }
 }
 
